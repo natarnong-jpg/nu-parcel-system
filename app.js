cbc8d61fa1de55b779046ea312a5de60b0881621
natarnong-jpg <natarnong@gmail.com>
2026-09-13 21:25:49 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('SW Registered!', reg))
+      .catch(err => console.error('SW Register Failed!', err));
+  });
+}
