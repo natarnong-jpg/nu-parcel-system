38e86c6d99e62eebf6276057c13f17a71acf7ddb
natarnong-jpg <natarnong@gmail.com>
2026-09-13 21:56:56 +0700

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    // ระบุเป็น path String ตรงๆ เพื่อไม่ให้ Parcel แปลงเป็น Blob
-    navigator.serviceWorker.register('./sw.js')
-      .then(reg => console.log('SW Registered successfully!', reg))
-      .catch(err => console.error('SW Registration failed:', err));
-  });
-}
