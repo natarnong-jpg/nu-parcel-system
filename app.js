44b74b909d63cfa31d1b151833f2f983f5b3ca15
natarnong-jpg <natarnong@gmail.com>
2026-09-13 21:30:42 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(reg => console.log('SW Registered!', reg))
-      .catch(err => console.error('SW Register Failed!', err));
+    // ใช้ new URL(...) เพื่อบอกให้ Parcel รู้ว่านี่คือไฟล์ภายนอก ไม่ต้องแปลงเป็น Blob
+    navigator.serviceWorker.register(
+      new URL('./sw.js', import.meta.url), 
+      { type: 'module' }
+    )
+    .then(reg => console.log('SW Registered successfully:', reg))
+    .catch(err => console.error('SW Registration failed:', err));
   });
 }
